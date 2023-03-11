60b6bf7086ce73eedfd3ca2d4672e19ca024a477
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-11 15:01:29 +0900

    preventDefault

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //html element를 자바스크립트로 가져오기
 const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input")
 
 
 function onLoginSubmit (tomato) {
+    //preventDefalut() 를 사용하여 기본적으로 submit할때 새로고침 되는 현상을 막음.
     tomato.preventDefault();
-    console.log(tomato);
+    console.log(loginInput.value);
 }
 
-
+//html의 Form 태그에서 submit 이벤트를 리슨함.
 loginForm.addEventListener("submit", onLoginSubmit)
