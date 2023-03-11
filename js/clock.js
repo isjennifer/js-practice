311ff3095d5c480625c6b64cca8e686a25107ce7
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-11 23:41:54 +0900

    시계 브라우저에 출력

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,12 @@
-const clock = document.querySelector("#clock")
+const htmlClock = document.querySelector("#clock")
 
-function getClock() {
+function clock() {
     const date = new Date();
-    console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+    htmlClock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 }
 
-setInterval(getClock, 1000)
+// clock()을 한번 먼저 실행해주면 처음에 html에서 설정해준 기본값인 00:00 이 나오지 않고 바로 시:분:초 가 나옴
+clock();
+setInterval(clock, 1000);
 
 // console에 1초마다 시:분:초 가 출력됨 = 시계 만듬!
