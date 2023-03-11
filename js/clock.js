2c0bf85775e0c69ec303ec38631a5819d8c2ccb8
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-11 23:56:50 +0900

    시계 시:분:초 모두 두자릿수로 만들기(한자리일경우 앞에 0 붙이기)

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,11 @@ const htmlClock = document.querySelector("#clock")
 
 function clock() {
     const date = new Date();
-    htmlClock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    // String(~) 또는 .toString() 으로 number->string으로 변경해야 padStart를 붙일수 있음 
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = date.getMinutes().toString().padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    htmlClock.innerText = `${hours}:${minutes}:${seconds}`
 }
 
 // clock()을 한번 먼저 실행해주면 처음에 html에서 설정해준 기본값인 00:00 이 나오지 않고 바로 시:분:초 가 나옴
