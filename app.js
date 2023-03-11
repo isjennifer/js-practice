e265746f081564b0868e9109931b5c2f2c7c128e
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-11 15:32:25 +0900

    getting username

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 //html element를 자바스크립트로 가져오기
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
+const greeting = document.querySelector("#greeting")
 
+const HIDDEN_CLASSNAME = "hidden"
 
-function onLoginSubmit (tomato) {
+function onLoginSubmit (event) {
     //preventDefalut() 를 사용하여 기본적으로 submit할때 새로고침 되는 현상을 막음.
-    tomato.preventDefault();
-    console.log(loginInput.value);
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = "Hello" + username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 //html의 Form 태그에서 submit 이벤트를 리슨함.
