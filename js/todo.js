f8dcbcf8d356f77cf025d76223dbbdcb34012cb5
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-14 21:03:47 +0900

    localstorage saving to load : 새로고침하면 없어지는 현상과 새로 쓰면 로컬에 덮어쓰는 현상 수정

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,15 @@ const toDoForm = document.getElementById("todo-form")
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.getElementById("todo-list")
 
+// 새로고침 한 후 입력칸에 새로운 값을 입력할때 localstorage에 있던 기존 값들이 없어지고 덮어씌워지는 현상
+// -> toDos 가 처음에 시작할때 빈 배열이기 때문. -> parsedToDos를 paint 하기 전에 toDos 배열에 넣어주기
+let toDos = [];
+const TODOS_KEY = "todos";
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     // 어떤 버튼을 눌렀고, 어떤 버튼의 li를 삭제해야하는지 모름 
     // event.target.부모요소 로 찾아내서 버튼이 눌려진 li 특정
@@ -25,7 +34,18 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    paintToDo(newToDo)
+    toDos.push(newToDo);
+    paintToDo(newToDo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit)
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+
+if (savedToDos !== null) {
+    // JSON.parse 로 string 형태인 savedToDos 를 array 형태로 바꿔줌.
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo)
+}
