bb642562919712717e6bfae60b8eabbbd36680a5
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-14 21:20:50 +0900

    Date.now()로 유저가 입력한 값에 랜덤한id 할당하기

--- js/todo.js
+++ js/todo.js
@@ -20,8 +20,9 @@ function deleteToDo(event) {
 
 function paintToDo(newToDo) {
     const li = document.createElement("li");
+    li.id = newToDo.id
     const span = document.createElement("span");
-    span.innerText = newToDo
+    span.innerText = newToDo.text
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo)
@@ -34,8 +35,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newToDo);
-    paintToDo(newToDo);
+    const newToDoObj = {
+        text: newToDo,
+        id: Date.now(),
+    }
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
     saveToDos();
 }
 
